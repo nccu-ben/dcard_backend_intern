4fd87cd35428859ddc66d8f1e8904ccd23e51b3f
nccu-ben <pohan.ho@gmail.com>
2022-02-26 00:09:08 +0800

    first part

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
 # dcard_backend_intern
 
-dcard Internship Application Requirements
+### Dcard Internship Application Requirements
 
 This program runs smoothly in the following environments:
     1.node.js --version v17.6.0
     2.npm     --version 8.5.1
+	3.npm install express mongoose shortid valid-url 
 
-The project contains two parts:
-    1.APIs
-    2.Test
+we'll need the following packages :
+	1.express : main libary for backend
+	2.mongoos : 
+	3.shortid : 
+	4.valid-url : 
 
-
-#First Part -- APIs
+## First Part -- APIs
 This program contains two api:
-​    1.upload
-​    2.redirect
-​ 
-#Second Part -- Test​    
+	1.upload
+    2.redirect
+
+## Second Part -- Test
 The test part have following files:
     1.test.py
     2.test.in
     2.test.out
+
+=================================================================
+
+How to Run Codes
+	1.npm install express mongoose shortid valid-url
+	2.node server.js
+	3.python test.py 
 
--- server.js
+++ server.js
@@ -1,9 +1,86 @@
 const express=require('express');
-const app = express();
-const port = 3000
+const validUrl = require('valid-url')
+const shortid = require('shortid')
 
-app.post('/api/vi/urls', (req,res)=>{
+// creating express route handler
+const router = express.Router()
+// import the Url database model
+const Url = require('../models/Url')
 
 
+router.post('/shorten', async (req, res) => {
+  const {
+        longUrl
+    } = req.body // destructure the longUrl from req.body.longUrl
+
+    // check base url if valid using the validUrl.isUri method
+    if (!validUrl.isUri(baseUrl)) {
+        return res.status(401).json('Invalid base URL')
+    }
+
+    // if valid, we create the url code
+    const urlCode = shortid.generate()
+
+    // check long url if valid using the validUrl.isUri method
+    if (validUrl.isUri(longUrl)) {
+        try {
+            /* The findOne() provides a match to only the subset of the documents 
+            in the collection that match the query. In this case, before creating the short URL,
+            we check if the long URL was in the DB ,else we create it.
+            */
+            let url = await Url.findOne({
+                longUrl
+            })
+
+            // url exist and return the respose
+            if (url) {
+                res.json(url)
+            } else {
+                // join the generated short code the the base url
+                const shortUrl = baseUrl + '/' + urlCode
+
+                // invoking the Url model and saving to the DB
+                url = new Url({
+                    longUrl,
+                    shortUrl,
+                    urlCode,
+                    date: new Date()
+                })
+                await url.save()
+                res.json(url)
+            }
+        }
+        // exception handler
+        catch (err) {
+            console.log(err)
+            res.status(500).json('Server Error')
+        }
+    } else {
+        res.status(401).json('Invalid longUrl')
+    }
 });
 
+router.get('/:code', async (req, res) => {
+    try {
+        // find a document match to the code in req.params.code
+        const url = await Url.findOne({
+            urlCode: req.params.code
+        })
+        if (url) {
+            // when valid we perform a redirect
+            return res.redirect(url.longUrl)
+        } else {
+            // else return a not found 404 status
+            return res.status(404).json('No URL Found')
+        }
+
+    }
+    // exception handler
+    catch (err) {
+        console.error(err)
+        res.status(500).json('Server Error')
+    }
+})
+
+
+module.exports = router
